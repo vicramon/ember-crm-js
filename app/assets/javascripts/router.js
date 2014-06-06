a51c92e337a10530b98c532ca7991dd91fd73224
Vic Ramon <vic@vicramon.com>
2014-06-06 13:17:02 -0400

    Prepare ember routes

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,4 +1,7 @@
-// For more information see: http://emberjs.com/guides/routing/
+App.Router.reopen({
+  location: 'auto',
+  rootURL: '/'
+})
 
 App.Router.map(function() {
   // this.resource('posts');
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+
+  get '*path', to: 'home#index'
 end
