a4da0343aec6a298163ec7ae5e5caa3c20c66a0f
Vic Ramon <vic@vicramon.com>
2014-06-06 13:32:21 -0400

    Setup ember layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,6 +7,6 @@
 //= require app
 
 // for more details see: http://emberjs.com/guides/application/
-App = Ember.Application.create();
+App = Ember.Application.create({rootElement: '#ember-app'});
 
 //= require_tree .
