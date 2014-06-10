c9175db64cd4255be73f49d368a8088beb2fdc30
Vic Ramon <vic@vicramon.com>
2014-06-10 10:42:09 -0400

    Searching leads

--- app/assets/javascripts/controllers/leads.js
+++ app/assets/javascripts/controllers/leads.js
@@ -1,3 +1,15 @@
 App.LeadsController = Ember.ArrayController.extend({
-  sortProperties: ['firstName', 'lastName']
+  sortProperties: ['firstName', 'lastName'],
+
+  leads: function() {
+    return this.get('search') ? this.get('searchedLeads') : this
+  }.property('search', 'searchedLeads'),
+
+  searchedLeads: function() {
+    var search = this.get('search').toLowerCase()
+    return this.filter(function(lead) {
+      return lead.get('fullName').toLowerCase().indexOf(search) != -1
+    })
+  }.property('search', 'this.each.fullName')
+
 })
