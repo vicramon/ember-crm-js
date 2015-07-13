adccfbd4e74350c692a3cab4cee48a26720fd2bd
Eric Guo <eric.guo@sandisk.com>
2015-07-13 21:27:58 +0800

    IE8 using delete as key words, so rename to destroy

--- app/assets/javascripts/controllers/lead_index.js
+++ app/assets/javascripts/controllers/lead_index.js
@@ -10,7 +10,7 @@ App.LeadIndexController = Ember.Controller.extend({
 
   actions: {
 
-    delete: function() {
+    destroy: function() {
       var self = this;
       this.get('model').destroyRecord().then(function() {
         self.transitionToRoute('leads');
